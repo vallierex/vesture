6f385a2f050b353a5b670ba6b5060e3ef09c1e76
vallierex <193371501+vallierex@users.noreply.github.com>
2025-04-20 14:18:14 +0200

    feat: configure base Axum application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Vesture");
+use axum::Router;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new();
+
+    let address = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Server running on http://{}", address);
+
+    axum::serve(tokio::net::TcpListener::bind(address).await.unwrap(), app)
+        .await
+        .unwrap();
 }
